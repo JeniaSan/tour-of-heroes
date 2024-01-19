c25511d8e3fa5b4112878413073b42b2dbf6cbbc
Evhenii Polishuchenko <sds191sds@gmail.com>
2024-01-19 13:05:00 +0200

    added navigation

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,6 +9,7 @@ import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 import {Hero} from '../hero';
 import {HEROES} from '../mock-heroes';
 import { HeroService } from '../hero.service';
+import { RouterModule, Routes } from '@angular/router';
 
 @Component({
   standalone: true,
@@ -20,26 +21,22 @@ import { HeroService } from '../hero.service';
     NgIf,
     NgFor,
     UpperCasePipe,
-    HeroDetailComponent
+    HeroDetailComponent,
+    RouterModule
   ],
 })
 
-export class HeroesComponent implements OnInit{
+export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
-  selectedHero?: Hero;
 
-  constructor(private heroService: HeroService) {}
-
-  private getHeroes(): void {
-    this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes);
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
     this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
   }
 }
